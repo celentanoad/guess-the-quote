352be5d5540a9d86a011fe35bcdb056aae65eabf
Alanna Celentano <celentanoad91@gmail.com>
2020-11-13 10:37:07 -0600

    add game logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import * as fakeQuotes from './data/fakeQuotes';
 import * as realQuotes from './data/realQuotes';
@@ -7,8 +7,10 @@ function App() {
  const [realQuote, setRealQuote] = useState(null)
  const [fakeQuote, setFakeQuote] = useState(null)
  const [order, setOrder] = useState(0)
+ const [message, setMessage] = useState(null)
 
  const getQuotes = async () => {
+   setMessage(null)
    const real = await realQuotes.getRandomRealQuote();
    const fake = await fakeQuotes.getRandomFakeQuote();
    setRealQuote(real);
@@ -17,25 +19,48 @@ function App() {
    setOrder(num);
  }
 
+ useEffect(() => {
+   getQuotes()
+ }, []);
 
-
+ const handleGuess = (e) => {
+   console.log(e)
+  if (e.target.textContent === realQuote) {
+    setMessage("You got it right!");
+  } else {
+    setMessage("Nope, that quote is fake!");
+  }
+ }
 
   return (
     <div className="App">
+    <h1>Did he <i>really</i> say that?</h1>
+    <h2>Click on the quote you think is real!</h2>
       <header className="App-header">
-        {order === 0 ?
-        <>
-        <button>{realQuote}</button>
-        <button>{fakeQuote}</button>
-        </>
+        {message ? 
+          <h3>{message}</h3>
+          :
+        order === 0 ?
+        <div className="buttons">
+          <button onClick={(e) => handleGuess(e)}>
+           <div className="quote">{realQuote}</div>
+          </button>
+          <button onClick={(e) => handleGuess(e)}>
+           <div className="quote">{fakeQuote}</div>
+          </button>
+        </div>
         :
-        <>
-        <button>{fakeQuote}</button>
-        <button>{realQuote}</button>
-        </>
-        }
-
-        <button onClick={getQuotes}>Get New Quotes</button>
+        <div className="buttons">
+           <button onClick={(e) => handleGuess(e)}>
+           <div className="quote">{fakeQuote}</div>
+          </button>
+          <button onClick={(e) => handleGuess(e)}>
+           <div className="quote">{realQuote}</div>
+          </button>
+        </div>
+        
+      }
+        <button className="new" onClick={getQuotes}>Get New Quotes</button>
       </header>
     </div>
   );
